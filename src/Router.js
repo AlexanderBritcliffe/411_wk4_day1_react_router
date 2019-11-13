70a1fd2ee6fe64676c77b6f0ce00ed0055bd397b
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2019-11-12 21:02:57 -0600

    got car details to render

--- src/Router.js
+++ src/Router.js
@@ -9,9 +9,24 @@ Remember to export the component at the end
 */
 
 import React from 'react'
-import { Switch, Route } from 'react-router'
+import { Switch, Route } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
+
+ 
 // Write component imports here //
 
 
 
 // Start Router function here //
+
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/About" component={About} />
+        </Switch>
+    )
+}
+
+export default Router
